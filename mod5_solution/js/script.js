b9c0c3c1abedd877f357563765df5ea1c01a89d1
aklimenx <aklimenx@gmail.com>
2016-06-12 12:17:49 -0400

    commit

--- mod5_solution/js/script.js
+++ mod5_solution/js/script.js
@@ -101,8 +101,8 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-       var chosenCategoryShortName = chooseRandomCategory(categories);
-      console.log(chosenCategoryShortName);
+       var chosenCategory = chooseRandomCategory(categories);
+      console.log(chosenCategory);
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -116,13 +116,17 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       // 
       // var homeHtmlToInsertIntoMainPage = ....
-      
+        var chosenCategoryShortName = chosenCategory.short_name;
+	    var homeHtmlToInsertIntoMainPage = 
+        insertProperty(homeHtmlToInsertIntoMainPage,
+                   "randomCategoryShortName",
+                   chosenCategoryShortName);
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that. 
       // ....
-      
+      insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
 }
@@ -132,7 +136,7 @@ function buildAndShowHomeHTML (categories) {
 function chooseRandomCategory (categories) {
   // Choose a random index into the array (from 0 inclusively until array length (exclusively))
   var randomArrayIndex = Math.floor(Math.random() * categories.length);
-	console.log(randomArrayIndex);
+
   // return category object with that randomArrayIndex
   return categories[randomArrayIndex];
 }
