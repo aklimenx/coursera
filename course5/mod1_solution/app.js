0386cf30227ec772e6fe0a20bf44608e66011cf0
aklimenx <aklimenx@gmail.com>
2017-07-23 17:48:39 -0400

    Module 1 Coding Assignment commit

--- course5/mod1_solution/app.js
+++ course5/mod1_solution/app.js
@@ -11,8 +11,35 @@ function Mod1AppController($scope, $filter) {
 
   $scope.checkIfTooMuch = function () {
     //return "Yaakov likes to eat healthy snacks at night!";
-    console.log("Too much!!!");
-    $scope.list = "was clicked";
+    console.log($scope.list);
+    //$scope.list = "was clicked";
+    var message;
+
+    if($scope.list === undefined || $scope.list === "")
+    {
+          message = "Please enter data first";
+    }
+    else
+    {
+      var message_size = $scope.list.split(',').length;
+
+
+      if ((message_size < 4) && (message_size > 0))
+      {
+        message = "Enjoy!";
+      }
+      else if(message_size == 0)
+      {
+        message = "Please enter data first";
+      }
+      else
+      {
+        message = "Too much!";
+      }
+
+    }
+
+    $scope.message = message;
   };
 /*
   $scope.feedYaakov = function () {
